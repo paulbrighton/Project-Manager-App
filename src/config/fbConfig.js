8523731ed3a0083ec8ad125d2396abd0ceb1fe01
Paul Brighton <brighton.paul@yahoo.com>
2019-08-02 12:24:00 +0100

    Firebase setup and sending/returning projects to database

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// App's Firebase configuration
+var config = {
+  apiKey: 'AIzaSyDhD6au07FB6oHSel6BqZQnlJcy0KvcZAA',
+  authDomain: 'pauls-project-manager.firebaseapp.com',
+  databaseURL: 'https://pauls-project-manager.firebaseio.com',
+  projectId: 'pauls-project-manager',
+  storageBucket: '',
+  messagingSenderId: '550186242850',
+  appId: '1:550186242850:web:0db2cf95cdfa427c'
+}
+// Initialize Firebase
+firebase.initializeApp(config)
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase
