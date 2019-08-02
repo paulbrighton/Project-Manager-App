8a283a1bdca72366b36c75743b5c834a13cebe6b
Paul Brighton <brighton.paul@yahoo.com>
2019-08-02 14:16:21 +0100

    authentication set up (#4)

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,26 @@
-const initState = {}
+const initState = {
+  authError: null
+}
 
 const authReducer = (state = initState, action) => {
-  return state
+  switch (action.type) {
+    case 'LOGIN_ERROR':
+      return {
+        ...state,
+        authError: 'Login failed'
+      }
+    case 'LOGIN_SUCCESS':
+      console.log('Login success')
+      return {
+        ...state,
+        authError: null
+      }
+    case 'LOGOUT_SUCCESS':
+      console.log('signout sucess')
+      return state
+    default:
+      return state
+  }
 }
 
 export default authReducer
