6aa20992ddb483b80d7ace58721d26053fc7cea1
Paul Brighton <brighton.paul@yahoo.com>
2019-08-05 15:20:54 +0100

    Develop to master (#10)
    * Added redux and thunk (#1)
    
    * Firebase setup and sending/returning projects to database (#2)
    
    * Added database data to project details page (#3)
    
    * authentication set up (#4)
    
    * Set up route guarding and added error message to sign up form (#5)
    
    * Added user info to projects (#6)
    
    * App working and deployed to firebase (#7)
    
    * Improved styling (#9)

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,15 +1,14 @@
 import React from 'react'
-// import moment from 'moment'
+import moment from 'moment'
 
 const ProjectSummary = ({project}) => {
   return (
     <div className='card z-depth-0 project-summary'>
       <div className='card-content grey-text text-darken-3'>
-        <span className='card-title'>{project.title}</span>
-        <span className='card-content'>{project.content}</span>
-        <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-        {/* This needs fixing */}
-        {/* <p className='grey-text'>{moment(project.createdAt.toDate()).calendar()}</p> */}
+        <span className='card-title'><p className='project-title'>{project.title}</p></span>
+        <span className='card-content grey-text darken-5'>{project.content}</span>
+        <p className='card-user dark-grey-text'>Posted by {project.authorFirstName} {project.authorLastName}</p>
+        <p className='grey-text'>{moment(project.createdAt.toDate()).calendar()}</p>
       </div>
     </div>
   )
