205996a66d71b709b95ec2b17d9d3c9a2795245b
Paul Brighton <brighton.paul@yahoo.com>
2019-08-05 15:11:34 +0100

    Improved styling

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -7,16 +7,17 @@ const Notifications = (props) => {
     <div className='section'>
       <div className='card z-depth-0'>
         <div className='card-content'>
-          <span className='card-title'>Notifications</span>
+          <span className='title'>Notifications</span>
           <ul className='notifications'>
             { notifications && notifications.map(item => {
               return (
-                <li key={item.id}>
+                <li className='notification-content' key={item.id}>
                   <span className='pink-text'>{item.user} </span>
-                  <span>{item.content}</span>
+                  <span className='dark-grey-text'>{item.content}</span>
                   <div className='grey-text note-date'>
                     {moment(item.time.toDate()).fromNow()}
                   </div>
+                  <div className='notification-divider' />
                 </li>
               )
             }) }
