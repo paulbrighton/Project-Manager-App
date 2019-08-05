a48b02b77edc002df4260cc25a22ea2d2c9fb2a6
Paul Brighton <brighton.paul@yahoo.com>
2019-08-05 12:59:23 +0100

    App working and deployed to firebase (#7)

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// import moment from 'moment'
 
 const ProjectSummary = ({project}) => {
   return (
@@ -7,7 +8,8 @@ const ProjectSummary = ({project}) => {
         <span className='card-title'>{project.title}</span>
         <span className='card-content'>{project.content}</span>
         <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-        <p className='grey-text'>1st August, 11:45am</p>
+        {/* This needs fixing */}
+        {/* <p className='grey-text'>{moment(project.createdAt.toDate()).calendar()}</p> */}
       </div>
     </div>
   )
