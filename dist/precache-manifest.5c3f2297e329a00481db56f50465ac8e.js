2d89b4ddfebb3b388a1708208f1821ca777ee069
Paul Brighton <brighton.paul@yahoo.com>
2019-08-05 12:57:57 +0100

    App working and deployed to firebase

---
+++ dist/precache-manifest.5c3f2297e329a00481db56f50465ac8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1c588c997fe25f0c159ba3afc02b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7042dd06fcc6f886387",
+    "url": "/static/css/main.f826ac95.chunk.css"
+  },
+  {
+    "revision": "eeea6e289db912cd6eb6",
+    "url": "/static/js/2.c6baf28c.chunk.js"
+  },
+  {
+    "revision": "c7042dd06fcc6f886387",
+    "url": "/static/js/main.44ab4e98.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
